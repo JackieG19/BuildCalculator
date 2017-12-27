d828bd678f3f4404426606917e69c8315494a67d
Jackie <jacgut19@gmail.com>
2017-12-27 18:33:50 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+function numbers(num){
+    var input = document.getElementById('input');
+    switch (num) {
+        case 1:
+            input.value += '1';
+            break;
+        case 2:
+            input.value += '2';
+            break;
+        case 3:
+            input.value += '3';
+            break;
+        case 4:
+            input.value += '4';
+            break;
+        case 5:
+            input.value += '5';
+            break;
+        case 6:
+            input.value += '6';
+            break;
+        case 7:
+            input.value += '7';
+            break;
+        case 8:
+            input.value += '8';
+            break;
+        case 9:
+            input.value += '9';
+            break;
+       
+    }
+}
