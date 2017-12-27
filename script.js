330cddf63ec44605e60823295bebb0cb77353d29
Jackie <jacgut19@gmail.com>
2017-12-27 19:45:41 +0000

    operators and backspace buttons

--- script.js
+++ script.js
@@ -35,8 +35,50 @@ function numbers(num){ //numbers are clickable
     }
 }
 
+// operators buttons
+function operators(ora){
+    var input = document.getElementById('input');
+    switch (ora) {
+        case '+':
+            input.value += '+';
+            break;
+        case '-':
+            input.value += '-';
+            break;
+        case 'x':
+            input.value += '*';
+            break;
+        case '/':
+            input.value += '/';
+            break;
+        case '+/-':
+            input.value += '-' + input.value;
+            break;
+       
+    }
+}
+
+
 // clear screen
 function clearScreen() {
     document.getElementById('input').value = "",
     document.getElementById('answer').value = "";
+}
+
+// backspace button
+function backSpace(num){
+    var input = document.getElementById('input');
+    var space = input.value;
+    if(space.length > 0){
+        space = space.substring(0, space.length-1) // removes the last number/operators entered
+        input.value = space;
+    }
+}
+
+// equal button
+function equal(num){
+    var input = document.getElementById('input');
+    var answer = Math.floor('='); 
+    document.getElementById('answer').value = '=' + answer;
+    console.log(equal)
 }
